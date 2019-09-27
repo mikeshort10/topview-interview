85d4cf37f2a1906ffc4e7264e5edd5f4bad3e840
Mike Short <mikeshort2014@gmail.com>
2019-09-26 23:37:31 -0400

    git update navbar

--- src/containers/Nav/index.tsx
+++ src/containers/Nav/index.tsx
@@ -4,24 +4,24 @@ import { Link } from 'react-router-dom';
 export class Nav extends React.Component {
   render(): JSX.Element {
     return (
-      <div className="navbar navbar-expand-md bg-dark">
+      <div className="navbar navbar-expand-md bg-dark navbar-dark">
         <div className="navbar-brand">
           <Link to="/">
             <i className="fas fa-bicycle" />
             {` Mike's Bikes`}
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#menu"
-            aria-controls="menu"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
         </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#menu"
+          aria-controls="menu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
         <div className="collapse navbar-collapse" id="menu">
           <ul className="navbar-nav">
             <li className="nav-item">
@@ -29,9 +29,16 @@ export class Nav extends React.Component {
                 Products
               </Link>
             </li>
-            <Link className="nav-link" to="/cart">
-              Cart
-            </Link>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cart">
+                Cart
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/checkout">
+                Checkout
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
